dd652137265b0f72f42b4024063230a1c241fb29
Om More <ommore@Oms-MacBook-Pro.local>
2026-01-14 10:27:59 -0600

    daily check in update and spelling correction in about section.

--- timeline.js
+++ timeline.js
@@ -16,6 +16,7 @@ const entries = [
     { date: "01.11.2026", desc: "Working on REST API Project (Go Lang).", link: "" },
     { date: "01.12.2026", desc: "REST API Project Completed", link: "" },
     { date: "01.13.2026", desc: "Applying Job's", link: "" },
+    { date: "01.14.2026", desc: "Applying jobs in a office enviornment!", link: "" },
 
     // adding my entries here.
 
